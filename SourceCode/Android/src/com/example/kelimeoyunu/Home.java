6449ab2c52ce0a2f259e323700028c173c93e6ec
Erdem Baş <erdembas64@gmail.com>
2013-09-19 00:04:53 +0300

    Andorid proje dosyası oluşturuldu....

---
+++ SourceCode/Android/src/com/example/kelimeoyunu/Home.java
@@ -0,0 +1,17 @@
+package com.example.kelimeoyunu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Home extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+	}
+
+	
+
+}
